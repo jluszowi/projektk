1fd6d564111251294acf09c05ebbf785c1e3120f
jluszowi <jluszowi@wszib.edu.pl>
2019-05-28 20:04:35 +0200

    Szkielet projektu

---
+++ src/main/java/pl/edu/wszib/projektk/ProjektkApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projektk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektkApplication.class, args);
+    }
+
+}
